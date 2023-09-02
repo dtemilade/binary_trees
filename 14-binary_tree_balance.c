af2f467f3c3d5f1b635daf50a6c434b82ad1a640
dtemilade <dtemilade@gmail.com>
2023-09-01 20:42:16 -0700

    updating commits

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_balance - Prototype to measure balance factor of a binary tree
  * @tree: Pointer to root node
@@ -24,4 +23,35 @@ return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 return (left_height - right_height);
+}
+/**
+ * binary_tree_height - prototype function to gets the height of a binary tree
+ * @tree: variable for root node to draw height from for tree
+ * Return: 0 on failure or NULL, the representing height on success.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	/*Introducing variable parameters*/
+	size_t left_size;
+	size_t right_size;
+
+	/*Assign values to variable parameters*/
+	left_size = 0;
+	right_size = 0;
+
+	/*Condition to return 0 or NULL*/
+	if (!tree)
+		return (0);
+
+	/*process to gets the height of a binary tree*/
+	if (tree->left)
+		left_size = left_size + 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_size = right_size + 1 + binary_tree_height(tree->right);
+
+	if (left_size < right_size)
+		return (right_size);
+	else
+		return (left_size);
 }
