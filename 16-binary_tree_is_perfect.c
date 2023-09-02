62cc4d5d9fb77adf528009be6bfae31b1aa367a4
dtemilade <dtemilade@gmail.com>
2023-09-02 07:48:53 -0700

    correction updated

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -88,9 +88,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	/*process to return result*/
-	if (!tree->right && !tree->left)
+	if (!tree->left && !tree->right)
 		return (1);
-	if (tree->right && tree->left)
+	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 	return (0);
